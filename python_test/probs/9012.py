ade82119a0bc7a28265caa991fa42522ac4cf9c1
dellyu03 <seunghwan081@gmail.com>
2022-10-17 02:26:59 +0900

    커밋 계정 확인

--- python_test/probs/9012.py
+++ python_test/probs/9012.py
@@ -1,11 +1,11 @@
 #my code
-import sys
-T = int(sys.stdin.readline().rstrip())
-stack = []
+# import sys
+# T = int(sys.stdin.readline().rstrip())
+# stack = []
 
-for _ in T:
-    to_examine = sys.stdin.readline().rstrip()
-    for element in to_examine:
-        if to_examine != 0:
+# for _ in T:
+#     to_examine = sys.stdin.readline().rstrip()
+#     for element in to_examine:
+#         if to_examine != 0:
 
-    
+#커밋 테스트용 변경점
